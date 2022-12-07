f35cf4b6475322a0921a4aecd078f8b850bc6ceb
David Espino <davidespinog@gmail.com>
2022-12-07 14:48:23 -0600

    starting claseVirtual model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class ClaseVirtual(models.Model):
+    title = models.CharField(max_length=30)
+    price = models.IntegerField()
+    image = models.ImageField(upload_to='uploads/')
+    
